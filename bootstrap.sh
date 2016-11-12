9ed6ae184140fb7793cf538b2421fd4e29c7ca4e
Keegan Lowenstein <keegan@bugsnag.com>
2016-11-11 16:53:42 -0800

    🐱

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
+
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude ".gitconfig" --exclude "README.md" --exclude ".gitignore" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bashrc
